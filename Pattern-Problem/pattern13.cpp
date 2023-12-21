e48e88258f1dea44d9c54cdf2fb428adaa6d0b65
devhimanshuu <devhimanshuu@gmail.com>
2023-12-21 14:47:39 +0530

    pattern13

---
+++ Pattern-Problem/pattern13.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print13(int n)
+{
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num = num + 1;
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; t++)
+    {
+        int n;
+        cin >> n;
+        print13(n);
+    }
+}
