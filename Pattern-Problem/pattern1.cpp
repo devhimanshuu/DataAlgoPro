61be80d761ce3852b1737eeaad0f3669f09a6e81
devhimanshuu <devhimanshuu@gmail.com>
2023-12-21 14:44:54 +0530

    pattern1

---
+++ Pattern-Problem/pattern1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; t++)
+    {
+        int n;
+        cin >> n;
+        print1(n);
+    }
+}
