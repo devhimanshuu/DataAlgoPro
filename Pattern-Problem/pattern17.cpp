d06bffefaa6226da26900c2cb916c6b530ec0b32
devhimanshuu <devhimanshuu@gmail.com>
2023-12-28 01:51:32 +0530

    pattern17

---
+++ Pattern-Problem/pattern17.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print17(int n)
+{
+
+    for (int i = 0; i <= n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; t++)
+    {
+        int n;
+        cin >> n;
+        print17(n);
+    }
+}
