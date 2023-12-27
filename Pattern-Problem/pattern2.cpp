b998ac6213d40e5a1e907c605344a91afbe63f1f
devhimanshuu <devhimanshuu@gmail.com>
2023-12-28 01:52:01 +0530

    pattern2

---
+++ Pattern-Problem/pattern2.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; t++)
+    {
+        int n;
+        cin >> n;
+        print2(n);
+    }
+}
