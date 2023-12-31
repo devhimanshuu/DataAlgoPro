8c4d5b3202bcd2c40ffea21e1876d4ee24f4c4a3
devhimanshuu <devhimanshuu@gmail.com>
2023-12-31 13:10:45 +0530

    pattern24

---
+++ Pattern-Problem/pattern24.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print24(int n)
+{
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; t++)
+    {
+        int n;
+        cin >> n;
+        print24(n);
+    }
+}
