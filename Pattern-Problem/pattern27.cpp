486cd598063345a5ed909a4c15c98e75e6e69b67
devhimanshuu <devhimanshuu@gmail.com>
2024-01-01 01:14:20 +0530

    pattern27

---
+++ Pattern-Problem/pattern27.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print25(int n)
+{
+
+    for (int i = 1; i <= n; i++)
+    {
+        // char ch = 'a' + (i - 1);
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; t++)
+    {
+        int n;
+        cin >> n;
+        print25(n);
+    }
+}
