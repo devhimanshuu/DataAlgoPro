d4cd8b8ff75c977afac94306fbb9f952832f53e8
devhimanshuu <devhimanshuu@gmail.com>
2024-01-02 02:20:04 +0530

    countDigits

---
+++ Basic_maths/countDigits.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int count_digits(int n)
+{
+    int x = n;
+    int count = 0;
+    while (x != 0)
+    {
+        x = x / 10;
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int n = 26726;
+    cout << "Number of digits in " << n << " is " << count_digits(n);
+    return 0;
+}
