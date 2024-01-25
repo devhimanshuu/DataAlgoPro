c3c59adf5122e8d3ba8c14ff5f8c4ca0f8f02803
devhimanshuu <devhimanshuu@gmail.com>
2024-01-25 14:12:35 +0530

    hashing3

---
+++ Hashing/hashing3.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // precompute:
+    map<int, int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int num;
+        cin >> num;
+        // fetch:
+        cout << mpp[num] << endl;
+    }
+    return 0;
+}
