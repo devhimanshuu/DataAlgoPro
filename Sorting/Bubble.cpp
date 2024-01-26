9e6d32e548056c582416b9cc5ffcc632375a53fd
devhimanshuu <devhimanshuu@gmail.com>
2024-01-26 16:39:11 +0530

    optimised

--- Sorting/Bubble.cpp
+++ Sorting/Bubble.cpp
@@ -5,6 +5,7 @@ void Bubble_sort(int arr[], int n)
 {
     for (int i = n - 1; i >= 0; i--)
     {
+        int didSwap = 0;
         for (int j = 0; j <= i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -12,8 +13,14 @@ void Bubble_sort(int arr[], int n)
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                didSwap = 1;
             }
         }
+        if (didSwap == 0)
+        {
+            break;
+        }
+        cout << "runs\n";
     }
 }
 int main()
