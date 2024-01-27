41918034f63a83a7c431fd2a0297328f8c4c23d0
devhimanshuu <devhimanshuu@gmail.com>
2024-01-28 00:40:55 +0530

    Quick_Sort

---
+++ Sorting/Quick.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+int partition(vector<int> &arr, int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while (i < j)
+    {
+        while (arr[i] <= pivot && i <= high - 1)
+        {
+            i++;
+        }
+        while (arr[j] > pivot && j >= low + 1)
+        {
+            j--;
+        }
+        if (i < j)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+void qs(vector<int> &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pIndex = partition(arr, low, high);
+        qs(arr, low, pIndex - 1);
+        qs(arr, pIndex + 1, high);
+    }
+}
+
+vector<int> quickSort(vector<int> arr)
+{
+    qs(arr, 0, arr.size() - 1);
+    return arr;
+}
+int main()
+{
+    vector<int> arr;
+    int n;
+    cout << "Enter the Size of the Array" << endl;
+    cin >> n;
+    cout << "Enter the elements of the array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+    cout << "Before Using quick Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    arr = quickSort(arr);
+    cout << "After Using quick sort: "
+         << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
