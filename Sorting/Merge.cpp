82130be3aa12816048cacb55ff8fd9052ec2c18b
devhimanshuu <devhimanshuu@gmail.com>
2024-01-27 15:22:17 +0530

    Merge_Sort

---
+++ Sorting/Merge.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;    // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1; // starting index of right half of arr
+
+    // storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);      // left half
+    mergeSort(arr, mid + 1, high); // right half
+    merge(arr, low, mid, high);    // merging sorted halves
+}
+
+int main()
+{
+
+    vector<int> arr;
+    int n;
+    cout << "Enter the Size of the Array" << endl;
+    cin >> n;
+    cout << "Enter the elements for the Array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+    cout << "Before Sorting Array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    mergeSort(arr, 0, n - 1);
+    cout << "After Sorting Array: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
