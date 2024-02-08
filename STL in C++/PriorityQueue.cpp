4487022a08af38c7c84a70b03e951b73c2623905
devhimanshuu <devhimanshuu@gmail.com>
2024-02-08 18:23:40 +0530

    PriorityQueue

---
+++ STL in C++/PriorityQueue.cpp
@@ -0,0 +1,30 @@
+/*In the case of the max heap, priority queues are a type of container adaptors, specifically designed such that its first element is always the greatest of the elements it contains and the rest elements are in decreasing order.
+
+In the case of the min heap, priority queues are a type of container adaptors, specifically designed such that its first element is always the smallest of the elements it contains and the rest elements are in increasing orders*/
+
+#include <bits/stdc++.h>
+using namespace std;
+void printpriorityqueue(priority_queue<int> pq)
+{
+    priority_queue<int> pq2 = pq;
+    while (!pq.empty())
+    {
+        cout << pq.top() << "\n";
+        pq.pop();
+    }
+}
+int main()
+{
+    priority_queue<int> pq;
+    for (int i = 1; i <= 5; i++)
+        pq.push(i);
+
+    cout << "The elements of the priority queue are:" << endl;
+    printpriorityqueue(pq);
+
+    cout << "The size of the priority queue: " << pq.size() << endl;
+    cout << "The top element of the priority queue: " << pq.top() << endl;
+    cout << "Pop the top element: " << endl;
+    pq.pop();
+    printpriorityqueue(pq);
+}
